b03d47982d66d6e2d0e4ba1c97f1e52ea030e10c
Boris Babic <babicbor@fit.cvut.cz>
2025-03-13 12:07:44 +0100

    pa2 hw01 fixed birth

--- PA2/hw01/register.cpp
+++ PA2/hw01/register.cpp
@@ -14,7 +14,7 @@
 #include <stdexcept>
 #include <algorithm>
 #endif /* __PROGTEST__ */
-/*
+
 class CIterator
 {
   public:
@@ -26,27 +26,30 @@ class CIterator
   private:
     // todo
 };
-*/
+
 class Person {
     private:
          std::string name;
          std::string addr;
          std::string account;
-         int balance;
+         int income;
+         int expense;
+
     public:
         Person( std::string n,
                 std::string ad,
-                std::string ac): name(n), addr(ad), account(ac), balance(0) {}
+                std::string ac): name(n), addr(ad), account(ac), income(0), expense(0) {}
         void print(){
           std::cout << "Osoba: " << this->name <<
                        " byva na " << this->addr <<
                        " s cislom uctu " << this->account <<
-                       "a na ucte ma " << this->balance << std::endl;
+                       " a na ucte ma prijem " << this->income <<
+                       " a na ucte ma odjem " << this->expense << std::endl;
         }
         bool operator == ( Person & other)  {
-          return (this->get_name() == other.get_name() &&
+          return ((this->get_name() == other.get_name() &&
                   this->get_address() == other.get_address()) ||
-                  this->get_account() == other.get_account();
+                  (this->get_account() == other.get_account()));
         }
         std::string get_name() const {
           return this->name;
@@ -57,8 +60,18 @@ class Person {
         std:: string get_address() const { 
           return this->addr;
         }
-        void change_balance(int amount) {
-          this->balance += amount;
+        int get_income() const{
+          return this->income;
+        }
+        int get_expense() const{
+          return this->expense;
+        }
+        void change_income(int amount) {
+          this->income += amount;
+          return;
+        }
+        void change_expense(int amount) {
+          this->expense += amount;
           return;
         }
         void change_name_addr(std::string n, std::string add) {
@@ -87,6 +100,21 @@ class CTaxRegister
   private:
     std::vector<Person> data_by_name;
     std::vector<Person> data_by_number;
+    std::vector<Person>::iterator get_iterator_name(Person guy) {
+      return std::lower_bound(data_by_name.begin(), data_by_name.end(), guy, compare_name);
+    }
+    std::vector<Person>::iterator get_iterator_account(Person guy) {
+      return std::lower_bound(data_by_number.begin(), data_by_number.end(), guy, compare_account);
+    }
+    bool person_in_vector_by_name(Person guy) {
+      return std::binary_search(data_by_name.begin(), data_by_name.end(), guy, compare_name);
+    }
+    bool person_in_vector_by_account(Person guy) {
+      return std::binary_search(data_by_number.begin(), data_by_number.end(), guy, compare_account);
+    }
+    bool person_in_data(Person guy) {
+      return (person_in_vector_by_name(guy) || person_in_vector_by_account(guy));
+    }
   public:
     CTaxRegister() {}
     bool      birth      ( const std::string    & name,
@@ -95,27 +123,15 @@ class CTaxRegister
     Person newborn(name, addr, account);
 
     //gets the position where to insert newborn in data_by_name
-    auto iterator_name = std::lower_bound(data_by_name.begin(),
-                                     data_by_name.end(),
-                                     newborn,
-                                     compare_name);
-    //same but in data_by_account
-    auto iterator_account = std::lower_bound(data_by_number.begin(),
-                                             data_by_number.end(),
-                                             newborn,
-                                            compare_account);
-    //checks if the person is already there, the first if is for unitialized iterator (caused segmentation fault)
-    if (iterator_name == data_by_name.end()) {
+    auto iterator_name = get_iterator_name(newborn);
+    auto iterator_account = get_iterator_account(newborn);
+    //checks if the person is already there
+    if (!person_in_data(newborn)) {
       data_by_name.insert(iterator_name, newborn);
       data_by_number.insert(iterator_account, newborn);
       return true;
-    }
-    else if (*iterator_name == newborn) {
-      return false;
     } else {
-      data_by_name.insert(iterator_name, newborn);
-      data_by_number.insert(iterator_account, newborn);
-      return true;
+      return false;
     }
     }
 
@@ -131,10 +147,11 @@ class CTaxRegister
                                               data_by_number.end(),
                                               deadguy,
                                               compare_account);
-    if (iterator_name == data_by_name.end()) {
+    if ((iterator_name == data_by_name.end())||  
+        ((*iterator_name).get_name() != deadguy.get_name() ||
+        (*iterator_name).get_address() != deadguy.get_address())) {
       return false;
-    }
-    else if (*iterator_name == deadguy) {
+    } else if (*iterator_name == deadguy) {
       data_by_name.erase(iterator_name);
       data_by_number.erase(iterator_account);
       return true;
@@ -149,7 +166,8 @@ class CTaxRegister
                                                 data_by_number.end(),
                                                 guy,
                                                 compare_account);
-      if (iterator_account == data_by_number.end()) {
+      if ((iterator_account == data_by_number.end()) ||
+          ((*iterator_account).get_account() != guy.get_account())) {
         return false;
       } else {
         guy.change_account((*iterator_account).get_account());
@@ -158,8 +176,8 @@ class CTaxRegister
                                      data_by_name.end(),
                                      guy,
                                      compare_name);
-        (*iterator_account).change_balance(amount);
-        (*iterator_name).change_balance(amount);
+        (*iterator_account).change_income(amount);
+        (*iterator_name).change_income(amount);
         return true;
       }
     }
@@ -173,7 +191,9 @@ class CTaxRegister
                                                 data_by_name.end(),
                                                 guy,
                                                 compare_name);
-      if (iterator_name == data_by_number.end()) {
+      if ((iterator_name == data_by_number.end()) || 
+          ((*iterator_name).get_name() != guy.get_name() ||
+           (*iterator_name).get_address() != guy.get_address())) {
         return false;
       } else {
         guy.change_account((*iterator_name).get_account());
@@ -182,21 +202,79 @@ class CTaxRegister
                                                 data_by_number.end(),
                                                 guy,
                                                 compare_account);
-        (*iterator_account).change_balance(amount);
-        (*iterator_name).change_balance(amount);
+        (*iterator_account).change_income(amount);
+        (*iterator_name).change_income(amount);
         return true;
       }
     }
     bool      expense    ( const std::string    & account,
-                           int                    amount );
+                           int                    amount ){
+      Person guy ("Jozko Mrkvicka", "nowhere", account);
+      auto iterator_account = std::lower_bound( data_by_number.begin(),
+                                                data_by_number.end(),
+                                                guy,
+                                                compare_account);
+      if ((iterator_account == data_by_number.end()) ||
+          ((*iterator_account).get_account() != guy.get_account())) {
+        return false;
+      } else {
+        guy.change_account((*iterator_account).get_account());
+        guy.change_name_addr((*iterator_account).get_name(), (*iterator_account).get_address());
+        auto iterator_name = std::lower_bound(data_by_name.begin(),
+                                      data_by_name.end(),
+                                      guy,
+                                      compare_name);
+        (*iterator_account).change_expense(amount);
+        (*iterator_name).change_expense(amount);
+        return true;
+      }
+    }
     bool      expense    ( const std::string    & name,
                            const std::string    & addr,
-                           int                    amount );
+                           int                    amount ) {
+      Person guy (name, addr, "null");
+      
+      auto iterator_name = std::lower_bound(data_by_name.begin(),
+                                                data_by_name.end(),
+                                                guy,
+                                                compare_name);
+      if ((iterator_name == data_by_number.end()) || 
+          ((*iterator_name).get_name() != guy.get_name() ||
+           (*iterator_name).get_address() != guy.get_address())) {
+        return false;
+      } else {
+        guy.change_account((*iterator_name).get_account());
+        guy.change_name_addr((*iterator_name).get_name(), (*iterator_name).get_address());
+        auto iterator_account = std::lower_bound( data_by_number.begin(),
+                                                data_by_number.end(),
+                                                guy,
+                                                compare_account);
+        (*iterator_account).change_expense(amount);
+        (*iterator_name).change_expense(amount);
+        return true;
+      }
+    }
     bool      audit      ( const std::string    & name,
                            const std::string    & addr,
                            std::string          & account,
                            int                  & sumIncome,
-                           int                  & sumExpense ) const;
+                           int                  & sumExpense ) const {
+      Person guy (name, addr, "null");
+      auto iterator_name = std::lower_bound(data_by_name.begin(),
+                                            data_by_name.end(),
+                                            guy,
+                                            compare_name);
+      if ((iterator_name == data_by_number.end()) || 
+          ((*iterator_name).get_name() != guy.get_name() ||
+          (*iterator_name).get_address() != guy.get_address())) {
+        return false;
+      } else {
+        sumIncome = (*iterator_name).get_income();
+        sumExpense = (*iterator_name).get_expense();
+        account = (*iterator_name) .get_account();
+        return true;
+      }
+    }
     void      print_by_name() {
       for (auto person :this->data_by_name) {
         person.print();
@@ -207,21 +285,21 @@ class CTaxRegister
         person.print();
       }
     }
-    //CIterator listByName () const;
+    CIterator listByName () const;
 };
 
 #ifndef __PROGTEST__
 int main ()
 {
-  //std::string acct;
-  //int    sumIncome, sumExpense;
+  std::string acct;
+  int    sumIncome, sumExpense;
   CTaxRegister b0;
   std::cout << "yespls" << std::endl;
   assert ( b0 . birth ( "John Smith", "Oak Road 23", "123/456/789" ) );
   assert ( b0 . birth ( "Jane Hacker", "Main Street 17", "Xuj5#94" ) );
   assert ( b0 . birth ( "Peter Hacker", "Main Street 17", "634oddT" ) );
   assert ( b0 . birth ( "John Smith", "Main Street 17", "Z343rwZ" ) );
-  assert ( !b0 . birth ( "John Smith", "Main Street 17", "Z3454Z" ) );
+  assert ( !b0 . birth ( "John Smith", "Main Street 17", "nah4Z" ) );
   assert ( !b0 . birth ( "Joasda Smith", "Main Street 17", "Z343rwZ" ) );
 
   b0.print_by_name();
@@ -243,11 +321,12 @@ int main ()
   b0.print_by_name();
   std::cout << "\n\n" << std::endl;
   b0.print_by_number();
-  /*
+
   assert ( b0 . expense ( "Jane Hacker", "Main Street 17", 2000 ) );
   assert ( b0 . expense ( "John Smith", "Main Street 17", 500 ) );
   assert ( b0 . expense ( "Jane Hacker", "Main Street 17", 1000 ) );
   assert ( b0 . expense ( "Xuj5#94", 1300 ) );
+
   assert ( b0 . audit ( "John Smith", "Oak Road 23", acct, sumIncome, sumExpense ) );
   assert ( acct == "123/456/789" );
   assert ( sumIncome == 3000 );
@@ -264,6 +343,7 @@ int main ()
   assert ( acct == "Z343rwZ" );
   assert ( sumIncome == 0 );
   assert ( sumExpense == 500 );
+  /*
   CIterator it = b0 . listByName ();
   assert ( ! it . atEnd ()
            && it . name () == "Jane Hacker"
@@ -288,7 +368,7 @@ int main ()
   assert ( it . atEnd () );
 
   assert ( b0 . death ( "John Smith", "Main Street 17" ) );
-
+  */
   CTaxRegister b1;
   assert ( b1 . birth ( "John Smith", "Oak Road 23", "123/456/789" ) );
   assert ( b1 . birth ( "Jane Hacker", "Main Street 17", "Xuj5#94" ) );
@@ -305,7 +385,7 @@ int main ()
   assert ( sumIncome == 0 );
   assert ( sumExpense == 0 );
   assert ( !b1 . birth ( "Joe Hacker", "Elm Street 23", "AAj5#94" ) );
- */
+  std::cout << "im done" <<std::endl;
   return EXIT_SUCCESS;
 }
 #endif /* __PROGTEST__ */
