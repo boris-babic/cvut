12475e528396210b916e9680595176d618b7d89f
Boris Babic <babicbor@fit.cvut.cz>
2025-04-16 11:10:51 +0200

    pa2 hw05 start

--- PA2/test1.cpp
+++ PA2/test1.cpp
@@ -17,12 +17,34 @@
 #include <memory>
 #include <compare>
 #include <complex>
+class CArray
+ {
+   public:
+    CArray ( int len )
+     {
+       m_D = new int[ m_L = len ];
+       for ( int i = 0; i < m_L; i ++ ) m_D[i] = 0;
+     }  
+    CArray & operator = ( const CArray src )
+     {
+       delete [] m_D;  
+       m_D = new int[ m_L = src . m_L ];
+       for ( int i = 0; i < m_L; i ++ ) m_D[i] = src . m_D[i];
+       return *this;
+     } 
+    ~CArray ( void ) { delete [] m_D; }
+    int & operator [] ( int i ) { return m_D[i]; }
+   private:   
+    int * m_D;
+    int   m_L;
+ };
 
-int main() {
-    std::vector<int> a;
-    a.resize(10);
-    a.insert(a.begin() + 5, 5);
-    a.shrink_to_fit();
-    return 0;
+int main ( void )
+ {
+   CArray a(51), b(12);
 
-}
+   b = a;
+   a[12] = 71;   
+   std::cout << b[12];
+   return 0;
+ }
