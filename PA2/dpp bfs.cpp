249edbfb00a77ea1bdd1bd3c7a58464d59441281
Boris Babic <babicbor@fit.cvut.cz>
2025-05-26 17:27:03 +0200

    exam bfs training correct start

--- PA2/dpp bfs.cpp
+++ PA2/dpp bfs.cpp
@@ -1,119 +1,184 @@
-#include <iostream>
-#include <vector>
-#include <memory>
-#include <stdexcept>
+#include <algorithm>
 #include <cassert>
-// ####################################################################
-// Forward declaration
-template <typename T_>
-class CMatrixView;
-// ####################################################################
-template <typename T_>
-class CMatrix
-{
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+ 
+class CTransport {
 public:
-    CMatrix(const size_t h, const size_t w) : m_H(h), m_W(w)
-    {
-        for (size_t i = 0; i < h; i++)
-            m_Data.emplace_back(m_W, T_());
-    }
+  // konstruktor
+  // destruktor
  
-    // to do
+  CTransport& addLine(std::istream& is);
  
-    // operator () to access elements
-    T_ & operator () (size_t row, size_t col)
-    {
-        if (row >= m_H || col >= m_W)
-            throw std::out_of_range("out of range");
-        return m_Data[row][col];
-    }
+  std::set<std::string> meetingPoints(
+    const std::vector<std::string>& fromList, int& sumCost
+  ) const;
  
-    CMatrixView<T_> createView (ssize_t rowOffset, ssize_t colOffset)
-    {
-        // to do
-    }
- 
-    size_t m_H;
-    size_t m_W;
-    std::vector<std::vector<T_>> m_Data;
+private:
+  // todo
 };
-// ####################################################################
-template <typename T_>
-class CMatrixView
-{
-    // to do
  
-    // operator () to access elements
-    T_ & operator () (ssize_t rowOffset, ssize_t colOffset)
-    {
-        // to do
-    }
+int main() {
+  CTransport t;
+  std::istringstream iss;
+  iss.clear();
+  iss.str(
+    "Newton\n"
+    "Black Hill\n"
+    "Wood Side\n"
+    "Green Hill\n"
+    "Lakeside\n"
+    "Newton\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Wood Side\n"
+    "Green Hill\n"
+    "Little Burnside\n"
+    "Great Newton\n"
+    "Wood Side\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Little Newton\n"
+    "Little Burnside\n"
+    "Castle Hill\n"
+    "Newton Crossroad\n"
+    "Lakeside Central\n"
+    "Little Newton\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Lakeside Central\n"
+    "Little Waterton\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Little Waterton\n"
+    "Waterton West\n"
+    "Waterton Central\n"
+    "Waterton East\n"
+    "Waterton Woods\n"
+    "Waterton North\n"
+    "Waterton East\n"
+    "Little Waterton\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Tidmouth\n"
+    "Gordon's Hill\n"
+    "Suderry\n"
+    "Knapford\n"
+    "Great Waterton\n"
+    "Brendam Docks\n"
+    "Tidmouth\n"
+  );
+  t.addLine(iss);
+  iss.clear();
+  iss.str(
+    "Sodor Ironworks\n"
+    "Sodor Steamwork\n"
+    "Knapford\n"
+    "Maron\n"
+    "Gordon's Hill\n"
+    "Sodor Ironworks\n"
+  );
+  t.addLine(iss);
  
-};
-// ####################################################################
-int main()
-{
-    bool exceptionThrown = false;
+  int cost;
+  assert(
+    (t.meetingPoints({"Newton"}, cost) ==
+     std::set<std::string> {"Newton"}) &&
+    (cost == 0)
+  );
  
-    CMatrix<int> m1(3, 3);
+  assert(
+    (t.meetingPoints({"Newton", "Newton"}, cost) ==
+     std::set<std::string> {"Newton"}) &&
+    (cost == 0)
+  );
  
-    m1(0, 0) = 100;
-    m1(0, 1) = 200;
-    m1(2, 2) = 400;
-    m1(1, 1) = 0;
+  assert(
+    (t.meetingPoints({"Brno"}, cost) == std::set<std::string> {"Brno"}
+    ) &&
+    (cost == 0)
+  );
  
-    assert(m1(0, 0) == 100);
-    assert(m1(1, 1) == 0);
-    assert(m1(2, 2) == 400);
+  assert(
+    (t.meetingPoints({"Brno", "Brno"}, cost) ==
+     std::set<std::string> {"Brno"}) &&
+    (cost == 0)
+  );
  
-    CMatrixView<int> v1 = m1.createView(1, 1);
+  assert(
+    (t.meetingPoints({"Newton", "Brno"}, cost) ==
+     std::set<std::string> {})
+  );
  
-    assert(v1(-1, -1) == 100);
-    v1(-1, -1) = 50;
-    assert(v1(-1, -1) == 50);
+  assert(
+    (t.meetingPoints({"Newton", "Black Hill"}, cost) ==
+     std::set<std::string> {"Newton", "Black Hill"}) &&
+    cost == 1
+  );
  
-    exceptionThrown = false;
-    try {
-        v1(2, 2) = 0;
-    } catch (const std::out_of_range &) {
-        exceptionThrown = true;
-    }
-    assert(exceptionThrown);
+  assert(
+    (t.meetingPoints({"Newton", "Newton", "Black Hill"}, cost) ==
+     std::set<std::string> {"Newton"}) &&
+    cost == 1
+  );
  
-    CMatrix<int> m2(4, 5);
+  assert(
+    (t.meetingPoints({"Newton", "Black Hill", "Wood Side"}, cost) ==
+     std::set<std::string> {"Black Hill"}) &&
+    cost == 2
+  );
  
-    m2(1, 3) = 100;
-    m2(0, 2) = 200;
-    m2(3, 4) = 400;
-    m2(0, 1) = 600;
+  assert(
+    (t.meetingPoints({"Newton Crossroad", "Little Waterton"}, cost) ==
+     std::set<std::string> {
+       "Newton Crossroad", "Little Waterton", "Lakeside Central"
+     }) &&
+    cost == 2
+  );
  
-    assert(m2(1, 3) == 100);
-    assert(m2(0, 1) == 600);
-    assert(m2(3, 4) == 400);
+  assert(
+    (t.meetingPoints({"Suddery", "Little Waterton"}, cost) ==
+     std::set<std::string> {})
+  );
  
-    CMatrixView<int> v2 = m2.createView(-2, 5);
+  assert(
+    (t.meetingPoints({"Tidmouth", "Newton Crossroad"}, cost) ==
+     std::set<std::string> {})
+  );
  
-    assert(v2(2, -3) == 200);
-    assert(v2(5, -1) == 400);
+  iss.clear();
+  iss.str(
+    "Newton\n"
+    "Newton Crossroad\n"
+    "Tidmouth\n"
+    "Newton\n"
+  );
+  t.addLine(iss);
  
-    CMatrixView<int> v3(v2);
- 
-    assert(v3(2, -3) == 200);
-    assert(v3(5, -1) == 400);
- 
-    v3 = v1;
- 
-    assert(v3(-1, -1) == 50);
- 
-    m2 = m1;
- 
-    exceptionThrown = false;
-    try {
-        v2(2, 2) = 0;
-    } catch (const std::logic_error &) {
-        exceptionThrown = true;
-    }
-    assert(exceptionThrown);
- 
-    return EXIT_SUCCESS;
+  assert(
+    (t.meetingPoints({"Tidmouth", "Newton Crossroad"}, cost) ==
+     std::set<std::string> {"Tidmouth", "Newton Crossroad"}) &&
+    cost == 1
+  );
 }
