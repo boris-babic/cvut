2fd564942477441b09b0bf060be98833c16551e1
Boris Babic <babicbor@fit.cvut.cz>
2025-10-18 17:09:43 +0200

    AG1 hw01 check 4 done

--- AG1/sunheart.cpp
+++ AG1/sunheart.cpp
@@ -101,13 +101,323 @@ CombatResult simulate_combat(Monster a, Monster b) {
   return *a_turns <= *b_turns ? A_WINS : B_WINS;
 }
 
+
+struct my_item {
+  my_item() = default;
+  my_item(const Item & source) {
+    this->first_attack = source.first_attack;
+    this->hp = source.hp;
+    this->off = source.off;
+    this->def = source.def;
+    this->stacking_def = source.stacking_def;
+    this->stacking_off = source.stacking_off;
+    this->stealth = source.stealth;
+  }
+  enum Type : uint8_t {
+    Weapon = 0,
+    Armor = 1,
+    RubberDuck = 2,
+    TYPE_COUNT = 3,
+  };
+  Type type = TYPE_COUNT;
+  int index = 0;
+  int hp = 0, off = 0, def = 0;
+  int stacking_off = 0, stacking_def = 0;
+  bool first_attack = false; // Hero attacks first.
+  bool stealth = false; // Hero can sneak past monsters (but cannot loot items while sneaking).
+  void print() const {
+    std::cout << "hp: " << hp << " off: " << off << " def: " << def << " soff: " << stacking_off << " sdef: " << stacking_def;
+    std::cout << " 1st_at" << first_attack << " stealth " << stealth << std::endl;
+  }
+  friend auto operator <=> (const my_item&, const my_item&) = default;
+};
+
+
+struct Player {
+    int hp = 10000, off = 3, def = 2;
+    int stacking_off = 0, stacking_def = 0;
+    std::array<my_item, 3> inventory;
+    Monster make_monster() {
+      Monster result;
+      result.hp = hp;
+      result.off = off;
+      result.def = def;
+      result.stacking_off = stacking_off;
+      result.stacking_def = stacking_def;
+
+      for (auto i : inventory) {
+        result.hp += i.hp;
+        result.off += i.off;
+        result.def += i.def;
+        result.stacking_off += i.stacking_off;
+        result.stacking_def += i.stacking_def;
+      }
+      if (result.hp <= 0) result.hp = 1;
+      return result;
+    }
+    bool has_stealth() {
+      for (auto i : inventory) {
+        if (i.stealth == true) return true;
+      }
+      return false;
+    }
+  };
+
+bool check_if_monster_defeatable(Player human, Monster monsteros) {
+  bool player_first = false;
+  for (auto i: human.inventory) {
+    if (i.first_attack == true) player_first = true;
+  }
+  if(player_first) {
+    auto result = simulate_combat(human.make_monster(), monsteros);
+    if (result == A_WINS) return true;
+    else return false;
+  } else {
+    auto result = simulate_combat(monsteros, human.make_monster());
+    if(result == B_WINS) return true;
+    else return false;
+  }
+}
+struct Node_State {
+  Node_State() = default;
+  RoomId node;
+  bool has_treasure = false;
+  std::array<my_item, 3> inventory = {};
+
+  void print() const {
+    std::cout << std::endl << "Node: " << std::endl; 
+    std::cout << "RoomId: " << this->node << " has treasure: " << (has_treasure ? "1": "0") << std::endl;
+    for (int i = 0; i < 3; i++) {
+      this->inventory[i].print();
+    }  
+  }
+  auto operator <=> (const Node_State& other) const noexcept = default;
+};
+
+void printAction(const Action &a) {
+    std::visit([](const auto &action) {
+        using T = std::decay_t<decltype(action)>;
+
+        if constexpr (std::is_same_v<T, Move>)
+            std::cout << "Move to room " << action.room << '\n';
+        else if constexpr (std::is_same_v<T, Pickup>)
+            std::cout << "Pickup item " << action.item << '\n';
+        else if constexpr (std::is_same_v<T, Drop>)
+            std::cout << "Drop item of type " << action.type << '\n';
+    }, a);
+}
+
+std::vector<Action> create_action_vector(const std::map<Node_State,std::pair<Node_State,Action>> & map, const Node_State & last, const Node_State & first) {
+  std::vector<Action> result;
+  Node_State current = last;
+
+  std::cout << "zacinam vyrabat result" <<std::endl;
+  while (current != first) {
+    current.print();
+    const auto &action = map.at(current).second;
+    if (!(std::holds_alternative<Drop>(action) && std::get<Drop>(action).type == Item::TYPE_COUNT)) {
+      result.push_back(action);
+    }
+    current = map.at(current).first;
+  }
+  std::reverse(result.begin(), result.end());
+  return result;
+}
 std::vector<Action> find_shortest_path(
   const std::vector<Room>& rooms,
   const std::vector<RoomId>& entrances,
   RoomId treasure
 ) {
   std::vector<Action> result;
+  Player human;
+  std::queue<Node_State> to_visit;
+  std::set<Node_State> visited;
+  std::map<Node_State,std::pair<Node_State,Action>> previous_action;
+  bool found_path = false;
+  Node_State first_room;
+  first_room.node = rooms.size();
+
+  std::cout << "zacinam bfs" << std::endl;
+  for (auto i : entrances) {
+    Node_State current_inserting; 
+    current_inserting.node = i;
+    previous_action[current_inserting] = std::make_pair(first_room, Move(i));
+    to_visit.push(current_inserting);
+  }
+  Node_State current_node_state;
+  std::cout << "zacinam while loop" << std::endl;
+  while(to_visit.size() > 0) {
+    //nacitam terajsi node
+    current_node_state = to_visit.front();
+    current_node_state.print();
+    //skontrolujem ci som tu uz neboljnjnjnsd as das 
+    if(std::find(visited.begin(), visited.end(), current_node_state) != visited.end()) {
+      std::cout << "uz som tu bol" << std::endl;
+      to_visit.pop();
+      continue;
+    }
+    to_visit.pop();
+    visited.emplace(current_node_state);
+    //testujem ci idem fightit
+    std::cout << "vyberam ci idem fightit" << std::endl;
+    if (!rooms[current_node_state.node].monster.has_value() || check_if_monster_defeatable(human, rooms[current_node_state.node].monster.value())) { //ci ho mozem fightit
+      //prekombinujem vsetky mozne possible inventories
+      std::cout << "zabil som priseru" << std::endl;
+      if (current_node_state.node == treasure) {
+        std::cout << "nasiel som poklad" << std::endl;
+        Node_State previous = current_node_state;
+        current_node_state.has_treasure = true;
+        previous_action[current_node_state] = std::make_pair(previous, Drop(Item::TYPE_COUNT));
+      }
+      if (current_node_state.has_treasure == true && std::find(entrances.begin(), entrances.end(), current_node_state.node) != entrances.end()) {
+        std::cout << "nasiel som cestu" << std::endl;
+        found_path = true;
+        break;
+      }
+      std::cout << "vyrabam vector itemov" << std::endl;
+      std::vector<std::vector<my_item>> weapons_available(3);
+      weapons_available[0].push_back(my_item());
+      weapons_available[1].push_back(my_item());
+      weapons_available[2].push_back(my_item());
+      // 0 je weapon 1 je armor 2 je duck
+      if (human.inventory[0] != my_item()) {
+        weapons_available[0].push_back(human.inventory[0]);
+      }
+      if (human.inventory[1] != my_item()) {
+        weapons_available[1].push_back(human.inventory[1]);
+      }
+      if (human.inventory[2] != my_item()) {
+        weapons_available[2].push_back(human.inventory[2]);
+      }
+
+      for (size_t i = 0; i < rooms[current_node_state.node].items.size(); i++) {
+        my_item current_item(rooms[current_node_state.node].items[i]);
+        current_item.index = i;
+        if(current_item.type == 0) weapons_available[0].push_back(current_item);
+        else if (current_item.type == 1) weapons_available[1].push_back(current_item);
+        else if (current_item.type == 2) weapons_available[2].push_back(current_item);
+      }
+      std::cout << "koniec vyrabania vector itemov" << std::endl;
+      //itemy spracovane idem spravit vsetky mozne kombinacie
+      Node_State working_state;
+      working_state.inventory = human.inventory;
+      working_state.node = current_node_state.node;
+      working_state.has_treasure = current_node_state.has_treasure;
+      Node_State previous_state;
+      previous_state.inventory = human.inventory;
+      previous_state.node = current_node_state.node;
+      previous_state.has_treasure = current_node_state.has_treasure;
+      ItemId id;
+      std::cout << "vchadzam do forloopu" << std::endl;
+      for (size_t weapon_index = 0; weapon_index < weapons_available[0].size(); weapon_index++) { //weapon
+        if ((weapon_index == 0) && (working_state.inventory[0] != my_item())) { //drop action
+          std::cout << "dropujem weapon" << std::endl;
+          working_state.inventory[0] = my_item();
+          previous_action[working_state] = std::make_pair(previous_state, Drop(Item::Weapon));
+          previous_state = working_state;
+        } else if (weapon_index != 0) { //pickup action
+          std::cout << "pickujem weapon" << std::endl;
+          working_state.inventory[0] = weapons_available[0][weapon_index];
+          id = working_state.inventory[0].index;
+          previous_action[working_state] = std::make_pair(previous_state, Pickup(id));
+          previous_state = working_state;
+        }
+        for (size_t armor_index = 0; armor_index < weapons_available[1].size(); armor_index++) { //armor
+          if ((armor_index == 0) && (working_state.inventory[1] != my_item())) { //drop action
+            std::cout << "dropujem armor" << std::endl;
+            working_state.inventory[1] = my_item();
+            previous_action[working_state] = std::make_pair(previous_state, Drop(Item::Armor));
+            previous_state = working_state;
+          } else if (armor_index != 0) { //pickup action
+            std::cout << "pickujem armor" << std::endl;
+            working_state.inventory[1] = weapons_available[1][armor_index];
+            id = working_state.inventory[1].index;
+            previous_action[working_state] = std::make_pair(previous_state, Pickup(id));
+            previous_state = working_state;
+          }
+          for (size_t duck_index = 0; duck_index < weapons_available[2].size(); duck_index++) { // duck
+            if ((duck_index == 0) && (working_state.inventory[2] != my_item())) { //drop action
+              std::cout << "dropujem duck" << std::endl;
+              working_state.inventory[2] = my_item();
+              previous_action[working_state] = std::make_pair(previous_state, Drop(Item::RubberDuck));
+              previous_state = working_state;
+            } else if (duck_index != 0) { //pickup action
+              std::cout << "pickujem duck" << std::endl;
+              working_state.inventory[2] = weapons_available[2][duck_index];
+              id = working_state.inventory[2].index;
+              previous_action[working_state] = std::make_pair(previous_state, Pickup(id));
+              previous_state = working_state;
+            }
+            for (auto i : rooms[current_node_state.node].neighbors) {
+              working_state.node = i;
+              previous_action[working_state] = std::make_pair(previous_state, Move(i));
+              to_visit.push(working_state);
+            }
+          }
+        }
+      }
+        
+      
+    } else if (human.has_stealth()) { //mozem stealthnut okolo 
+      //tu vlozim vsetkych neighbours s terajsim inventarom alebo dropnutym inventarom
+      std::cout << "stealthujem okolo" << std::endl;
+      for (auto i : rooms[current_node_state.node].neighbors) {
+              //nedropnem nic
+              Node_State working_state = current_node_state;
+              to_visit.push(working_state);
+              //dropnem len 1 item
+              for (int slot = 0; i < 3; slot++) {
+                working_state.inventory[slot] = my_item();
+                to_visit.push(working_state);
+                working_state.inventory[slot] = current_node_state.inventory[slot];
+              }
+              //dropnem 2 itemy
+              working_state.inventory[0] = my_item();
+              working_state.inventory[1] = my_item();
+              to_visit.push(working_state);
+              working_state.inventory[0] = current_node_state.inventory[0];
+              working_state.inventory[1] = current_node_state.inventory[1];
+
+              working_state.inventory[0] = my_item();
+              working_state.inventory[2] = my_item();
+              to_visit.push(working_state);
+              working_state.inventory[0] = current_node_state.inventory[0];
+              working_state.inventory[2] = current_node_state.inventory[2];
+
+              working_state.inventory[1] = my_item();
+              working_state.inventory[2] = my_item();
+              to_visit.push(working_state);
+              working_state.inventory[1] = current_node_state.inventory[1];
+              working_state.inventory[2] = current_node_state.inventory[2];
+              //dropnem vsetky itemy
+              working_state.inventory[0] = my_item();
+              working_state.inventory[1] = my_item();
+              working_state.inventory[2] = my_item();
+              to_visit.push(working_state);
+      }
+    } else {
+      continue; //nemam tu co robit
+    } 
+
+  }
+
+  std::cout << "bfs dobehlo" << std::endl;
+  if (found_path) {
+    std::cout << "mam cestu" << std::endl;
+    for (const auto & pair : previous_action) {
+      std::cout << "-------------------------------key:" << std::endl;
+      pair.first.print();
+      std::cout << "value:" << std::endl;
+      pair.second.first.print();
+      std::cout << "action: " << std::endl;
+      printAction(pair.second.second);
+      std::cout << "-----------------------------------" << std::endl;
+    }
+    result = create_action_vector(previous_action, current_node_state, first_room);
+  } 
+  std:: cout << "koncim find shortest path" << std::endl;
   return result;
+
 }
 
 #ifndef __PROGTEST__
@@ -125,7 +435,7 @@ void check_solution(
   const std::vector<RoomId>& entrances,
   RoomId treasure,
   size_t expected_rooms,
-  bool print = false
+  bool print = true
 ) {
   // TODO check if hero survives combat
   // TODO check if treasure was collected
@@ -219,33 +529,35 @@ void combat_examples() {
   std::vector<Room> rooms(2);
   rooms[0].neighbors.push_back(1);
   rooms[1].neighbors.push_back(0);
-
+  std::cout << "zacinam checkovati " << std::endl;
   check_solution(rooms, { 0 }, 1, 3);
+  std::cout << "check 1 done " << std::endl;
 
   rooms[1].monster = Monster{ .hp = 9'999, .off = 3, .def = 2 };
   check_solution(rooms, { 0 }, 1, 3);
-  
+  std::cout << "check 2 done " << std::endl;
   rooms[1].monster->hp += 1;
   check_solution(rooms, { 0 }, 1, 0);
-
+  std::cout << "check 3 done " << std::endl;
   rooms[1].monster = Monster{ .hp = 100'000, .off = 10 };
   check_solution(rooms, { 0 }, 1, 0);
-
+  std::cout << "check 4 done " << std::endl;
   rooms[0].items = { defensive_duck };
   check_solution(rooms, { 0 }, 1, 3);
-
+  std::cout << "check 5 done " << std::endl;
   rooms[0].items = { invincible_duck };
   check_solution(rooms, { 0 }, 1, 3);
-
+  std::cout << "check 6 done " << std::endl;
   rooms[0].items = {};
   rooms[1].monster = Monster{ .hp=1, .off=3, .def=0, .stacking_def=100 };
   check_solution(rooms, { 0 }, 1, 0);
-
+  std::cout << "check 7 done " << std::endl;
   rooms[0].items.push_back(offensive_duck);
   check_solution(rooms, { 0 }, 1, 0);
-
+  std::cout << "check 8 done " << std::endl;
   rooms[0].items.push_back(fast_duck);
   check_solution(rooms, { 0 }, 1, 3);
+  std::cout << "check 9 done " << std::endl;
 }
 
 void stealth_examples() {
@@ -542,8 +854,8 @@ void example_tests5() {
 
 int main() {
   std::cout << "letsgooooo" << std::endl;
-  /*
   combat_examples();
+  /*
   stealth_examples();
   example_tests();
   example_tests2();
@@ -551,7 +863,7 @@ int main() {
   example_tests4();
   example_tests5();
   */
-  std::cout << "koncim" << std::endl;
+ std::cout << "koncim" << std::endl;
 }
 
 #endif
